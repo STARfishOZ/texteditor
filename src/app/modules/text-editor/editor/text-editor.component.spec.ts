2fbc6d1d36c09e6f48bcf952636eb32e5f7a83dc
Andrii Davis <andriy.davis@partner.jambit.com>
2018-09-24 10:08:44 +0300

    Added tests to text editor component

--- src/app/modules/text-editor/editor/text-editor.component.spec.ts
+++ src/app/modules/text-editor/editor/text-editor.component.spec.ts
@@ -2,29 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import * as components from 'src/app/modules/text-editor';
 import * as pipes from 'src/app/pipes';
 import { TextEditorService } from '../services/text-editor.service';
-import {DataMuseApi, RandomTextApi} from '../../api-clients';
-import {ToastrService} from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 import {of} from 'rxjs';
+import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
+import * as textEditorPack from '../index';
 
-xdescribe('TextEditorComponent', () => {
+describe('TextEditorComponent', () => {
   let component: components.TextEditorComponent;
   let fixture: ComponentFixture<components.TextEditorComponent>;
-  let dataMuseApiSpy: jasmine.SpyObj<DataMuseApi>;
-  let randomTextApiSpy: jasmine.SpyObj<RandomTextApi>;
   let toastrServiceSpy: jasmine.SpyObj<ToastrService>;
+  let textEditorServiceSpy: jasmine.SpyObj<TextEditorService>;
 
   beforeEach(async(() => {
 
-    dataMuseApiSpy = jasmine.createSpyObj('DataMuseApi', ['getSynonymsList']);
-    randomTextApiSpy = jasmine.createSpyObj('RandomTextApi', ['getRandomText']);
     toastrServiceSpy = jasmine.createSpyObj('ToastrService', ['success', 'info', 'error']);
-
-    const randomTextService = {
-      getRandomText() {
-        const todos = [{id: 1}];
-        return of( todos );
-      }
-    };
+    textEditorServiceSpy = jasmine.createSpyObj('TextEditorService', ['getRandomText', 'getSynonymsListForSelection']);
 
     TestBed.configureTestingModule({
       declarations: [
@@ -34,31 +26,55 @@ xdescribe('TextEditorComponent', () => {
         pipes.StripHtmlPipe
       ],
       providers: [
-        TextEditorService,
         {
-          provide: DataMuseApi,
-          useValue: dataMuseApiSpy
-        },
-        {
-          provide: RandomTextApi,
-          useValue: randomTextService
+          provide: TextEditorService,
+          useValue: textEditorServiceSpy
         },
         {
           provide: ToastrService,
           useValue: toastrServiceSpy
         }
       ]
-    })
-      .compileComponents();
+    });
+
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [textEditorPack.ColorPaletteComponent]
+      }
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(components.TextEditorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(components.TextEditorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  describe('ngOnInit', () => {
+
+    it('should init component', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should call textEditorService getRandomText method', () => {
+      textEditorServiceSpy.getRandomText.and.returnValue(of(['asd']));
+
+      expect(textEditorServiceSpy.getRandomText)
+        .toHaveBeenCalledTimes(1);
+    });
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('getSynonyms', () => {
+
+    it('should call textEditorService getSynonymsListForSelection method', () => {
+      textEditorServiceSpy.getSynonymsListForSelection.and.returnValue(of(['asd']));
+      textEditorServiceSpy.getSynonymsListForSelection('Something', 5);
+
+      expect(textEditorServiceSpy.getSynonymsListForSelection)
+        .toHaveBeenCalledTimes(1);
+      expect(textEditorServiceSpy.getSynonymsListForSelection)
+        .toHaveBeenCalledWith('Something', 5);
+    });
+
   });
 });
